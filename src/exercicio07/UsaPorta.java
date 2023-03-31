24e874eac01eb7f160a633c2e1d37d9fa4220bf1
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-31 12:10:54 -0300

    Termino do exercício 8, faltam o 9 e o 10

--- src/exercicio07/UsaPorta.java
+++ src/exercicio07/UsaPorta.java
@@ -5,7 +5,11 @@ public class UsaPorta {
 	public static void main(String[] args) {
 		Porta corredor[] = new Porta[20];
 		
-		
+		int k = 0;
+		for (int i = 700; i < 705; i++) {
+			corredor[k] = new Porta(i);
+			k++;
+		}
 
 	}
 
